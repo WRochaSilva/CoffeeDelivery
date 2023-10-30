9b6ab6e6906b93fb6cb2410be987fb07f1e80644
Wesley <wesrocha26@gmail.com>
2023-10-30 18:13:24 -0300

    create database coffees

--- src/Context/GlobalContext.tsx
+++ src/Context/GlobalContext.tsx
@@ -1,18 +1,6 @@
 import { createContext, useContext, useState } from "react";
-import ExpressoTradicional from "../assets/ExpressoTradicional.png";
-import ExpressoAmaricano from "../assets/ExpressoAmaricano.png";
-import ExpressoCremoso from "../assets/ExpressoCremoso.png";
-import ExpressoGelado from "../assets/ExpressoGelado.png";
-import CafeComLeite from "../assets/CafeComLeite.png";
-import Latte from "../assets/Latte.png";
-import Capuccino from "../assets/Capuccino.png";
-import Macchiato from "../assets/Macchiato.png";
-import Mocaccino from "../assets/Mocaccino.png";
-import ChocolateQuente from "../assets/ChocolateQuente.png";
-import Cubano from "../assets/Cubano.png";
-import Havaiano from "../assets/Havaiano.png";
-import Arabe from "../assets/Arabe.png";
-import Irlandes from "../assets/Irlandes.png";
+import { DataBaseCoffees } from "../DataBaseCoffees";
+
 
 type Tmenu = {
   id: number;
@@ -46,125 +34,7 @@ type TGlobalProps = {
 const CtxGlobal = createContext({} as TGlobalProps);
 
 export const GlobalContext = ({ children }: { children: JSX.Element }) => {
-  const [coffees, setCoffees] = useState<Tmenu[]>([
-    {
-      id: 1,
-      name: "Expresso Tradicional",
-      photo: ExpressoTradicional,
-      description: "O tradicional café feito com água quente e grãos moídos",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 2,
-      name: "Expresso Americano",
-      photo: ExpressoAmaricano,
-      description: "Expresso diluído, menos intenso que o tradicional",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 3,
-      name: "ExpressoCremoso",
-      photo: ExpressoCremoso,
-      description: "Café expresso tradicional com espuma cremosa",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 4,
-      name: "ExpressoGelado",
-      photo: ExpressoGelado,
-      description: "Bebida preparada com café expresso e cubos de gelo",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 5,
-      name: "CafeComLeite",
-      photo: CafeComLeite,
-      description: "Meio a meio de expresso tradicional com leite vaporizado",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 6,
-      name: "Latte",
-      photo: Latte,
-      description:
-        "Uma dose de café expresso com o dobro de leite e espuma cremosa",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 7,
-      name: "Capuccino",
-      photo: Capuccino,
-      description:
-        "Bebida com canela feita de doses iguais de café, leite e espuma",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 8,
-      name: "Macchiato",
-      photo: Macchiato,
-      description:
-        "Café expresso misturado com um pouco de leite quente e espuma",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 9,
-      name: "Mocaccino",
-      photo: Mocaccino,
-      description: "Café expresso com calda de chocolate, pouco leite e espuma",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 10,
-      name: "ChocolateQuente",
-      photo: ChocolateQuente,
-      description:
-        "Bebida feita com chocolate dissolvido no leite quente e café",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 11,
-      name: "Cubano",
-      photo: Cubano,
-      description:
-        "Drink gelado de café expresso com rum, creme de leite e hortelã",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 12,
-      name: "Havaiano",
-      photo: Havaiano,
-      description: "Bebida adocicada preparada com café e leite de coco",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 13,
-      name: "Arabe",
-      photo: Arabe,
-      description: "Bebida preparada com grãos de café árabe e especiarias",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-    {
-      id: 14,
-      name: "Irlandes",
-      photo: Irlandes,
-      description: "Bebida a base de café, uísque irlandês, açúcar e chantilly",
-      price: 9.99,
-      orderQuantity: 0,
-    },
-  ]);
+  const [coffees, setCoffees] = useState<Tmenu[]>(DataBaseCoffees);
 
   const [dataDelivery, setDataDelivery] = useState<TDelivery | undefined>(
     undefined
--- src/Pages/OrderConfirmed/index.tsx
+++ src/Pages/OrderConfirmed/index.tsx
@@ -1,9 +1,9 @@
 import { Flex, Icon, Img, Text } from "@chakra-ui/react";
 import sucess from "../../assets/sucess.svg";
 import { useGlobal } from "../../Context/GlobalContext";
-import {GoLocation} from "react-icons/go"
-import {PiTimer} from "react-icons/pi"
-import {RiMoneyDollarCircleLine} from "react-icons/ri"
+import { GoLocation } from "react-icons/go";
+import { PiTimer } from "react-icons/pi";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
 export const OrderConfirmed = () => {
   const { dataDelivery } = useGlobal();
@@ -17,33 +17,39 @@ export const OrderConfirmed = () => {
       </Flex>
       <Flex alignItems={"center"}>
         <Flex
-         flexDir={'column'}
+          flexDir={"column"}
           w={"470px"}
           h={"160px"}
           borderRadius={"6px 36px"}
           border={"1px solid #DBAC2C"}
-          pl={'20px'}
-          justifyContent={'center'}
+          pl={"20px"}
+          justifyContent={"center"}
         >
           <Flex>
-            <Icon as={GoLocation} boxSize={'40px'} mr={'5px'}/>
-            <Flex  flexDir={'column'}>
+            <Icon as={GoLocation} boxSize={"40px"} mr={"5px"} />
+            <Flex flexDir={"column"}>
               <Text>{`Entrega em ${dataDelivery?.rua}, ${dataDelivery?.numero}`}</Text>
               <Text>{`${dataDelivery?.bairro} - ${dataDelivery?.cidade}, ${dataDelivery?.uf}`}</Text>
             </Flex>
           </Flex>
           <Flex>
-            <Icon as={PiTimer} boxSize={'40px'} mr={'5px'}/>
-            <Flex flexDir={'column'}>
-            <Text>Previsão de entrega</Text>
-            <Text>20 min - 30 min</Text>
+            <Icon as={PiTimer} boxSize={"40px"} mr={"5px"} />
+            <Flex flexDir={"column"}>
+              <Text>Previsão de entrega</Text>
+              <Text>20 min - 30 min</Text>
             </Flex>
           </Flex>
           <Flex>
-            <Icon as={RiMoneyDollarCircleLine} boxSize={'40px'} mr={'5px'}/>
-            <Flex flexDir={'column'}>
-            <Text>Pagamento na entrega</Text>
-            <Text>{dataDelivery?.payment === 0 ? 'Cartão de crédito' : dataDelivery?.payment === 1 ? 'Cartão de débito' : ''}</Text>
+            <Icon as={RiMoneyDollarCircleLine} boxSize={"40px"} mr={"5px"} />
+            <Flex flexDir={"column"}>
+              <Text>Pagamento na entrega</Text>
+              <Text>
+                {dataDelivery?.payment === 0
+                  ? "Cartão de crédito"
+                  : dataDelivery?.payment === 1
+                  ? "Cartão de débito"
+                  : "Dinheiro"}
+              </Text>
             </Flex>
           </Flex>
         </Flex>
