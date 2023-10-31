86b3aa03eb22eb1795726edc7fe59d75c22b12db
Wesley <wesrocha26@gmail.com>
2023-10-31 18:11:37 -0300

    update layout header

--- src/Pages/Header/index.tsx
+++ src/Pages/Header/index.tsx
@@ -1,7 +1,7 @@
 import { Button, Flex, Icon, Img, Text } from "@chakra-ui/react";
 import Logo from "../../assets/Logo.svg";
-import { CiLocationOn } from "react-icons/ci";
-import { AiOutlineShoppingCart } from "react-icons/ai";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaShoppingCart } from "react-icons/fa";
 import { useGlobal } from "../../Context/GlobalContext";
 import { useNavigate } from "react-router-dom";
 
@@ -9,31 +9,48 @@ export const Header = () => {
   const { totalCoffees } = useGlobal();
   const navigate = useNavigate();
   return (
-    <Flex justifyContent={"space-between"} mb={"40px"}>
+    <Flex justifyContent={"space-between"} m={"20px"}>
       <Img src={Logo} />
       <Flex alignItems={"start"}>
-        <Flex gap={"5px"} alignItems={"center"}>
-          <Icon as={CiLocationOn} />
-          <Text mr={"20px"}>Londrina-PR</Text>
+        <Flex
+          borderRadius={'6px'}
+          w={"127px"}
+          h={'25px'}
+          bgColor={'#EBE5F9'}
+          gap={"4px"}
+          mr={'5px'}
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+          <Icon as={FaLocationDot} color={'#4B2995'}/>
+          <Text
+            fontFamily="Roboto, sans-serif"
+            fontSize={"14px"}
+            color={'#4B2995'}
+            mr={"20px"}
+          >
+            Londrina-PR
+          </Text>
         </Flex>
         <Flex w={"60px"} h={"60px"}>
           <Button
-            w={"40px"}
-            h={"40px"}
-            leftIcon={<AiOutlineShoppingCart />}
+            w={"25px"}
+            h={"25px"}
+            bgColor={'#F1E9C9'}
+            leftIcon={<FaShoppingCart color={'#C47F17'}/>}
             onClick={() => navigate("/checkout")}
           />
           <Flex
             w={"15px"}
             h={"15px"}
-            m={"30px 0px 0px -8px"}
-            bgColor="red"
+            m={"-8px 0px 0px -8px"}
+            bgColor={'#C47F17'}
             borderRadius={"10px"}
             alignItems={"center"}
             justifyContent={"center"}
             zIndex={100}
           >
-            {totalCoffees}
+            <Text color={'white'} fontSize={'10px'}>{totalCoffees}</Text>
           </Flex>
         </Flex>
       </Flex>
