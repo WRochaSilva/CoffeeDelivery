9b9ad684df8d1184d8746d9deb89e5c577021bc2
Wesley <wesrocha26@gmail.com>
2023-11-08 09:41:01 -0300

    update layout orderConfirmed

--- src/Pages/OrderConfirmed/index.tsx
+++ src/Pages/OrderConfirmed/index.tsx
@@ -1,9 +1,9 @@
 import { Flex, Grid, GridItem, Icon, Img, Text } from "@chakra-ui/react";
 import sucess from "../../assets/sucess.svg";
 import { useGlobal } from "../../Context/GlobalContext";
-import { GoLocation } from "react-icons/go";
-import { PiTimer } from "react-icons/pi";
-import { RiMoneyDollarCircleLine } from "react-icons/ri";
+import { FaLocationDot } from "react-icons/fa6";
+import { RiTimerFill } from "react-icons/ri";
+import { BiSolidDollarCircle } from "react-icons/bi";
 
 export const OrderConfirmed = () => {
   const { dataDelivery } = useGlobal();
@@ -31,26 +31,70 @@ export const OrderConfirmed = () => {
           borderRadius={"6px 36px"}
           border={"1px inset #DBAC2C"}
           pl={"20px"}
+          gap={"10px"}
           justifyContent={"center"}
         >
-          <Flex>
-            <Icon as={GoLocation} boxSize={"40px"} mr={"5px"} />
+          <Flex alignItems={"center"}>
+            <Icon
+              as={FaLocationDot}
+              boxSize={"25px"}
+              bgColor={"#8047F8"}
+              color={"white"}
+              borderRadius={"20px"}
+              mr={"5px"}
+              p={"4px"}
+            />
             <Flex flexDir={"column"}>
-              <Text>{`Entrega em ${dataDelivery?.rua}, ${dataDelivery?.numero}`}</Text>
-              <Text>{`${dataDelivery?.bairro} - ${dataDelivery?.cidade}, ${dataDelivery?.uf}`}</Text>
+              <Flex>
+                <Text
+                  fontFamily="Roboto, sans-serif"
+                  fontSize={"16px"}
+                >Entrega em &nbsp;</Text>
+                <Text
+                  as={"b"}
+                  fontFamily="Roboto, sans-serif"
+                  fontSize={"16px"}
+                >{`${dataDelivery?.rua}, ${dataDelivery?.numero}`}</Text>
+              </Flex>
+              <Text
+                fontFamily="Roboto, sans-serif"
+                fontSize={"16px"}
+              >{`${dataDelivery?.bairro} - ${dataDelivery?.cidade}, ${dataDelivery?.uf}`}</Text>
             </Flex>
           </Flex>
-          <Flex>
-            <Icon as={PiTimer} boxSize={"40px"} mr={"5px"} />
+          <Flex alignItems={"center"}>
+            <Icon
+              as={RiTimerFill}
+              boxSize={"25px"}
+              bgColor={"#DBAC2C"}
+              color={"white"}
+              borderRadius={"20px"}
+              mr={"5px"}
+              p={"4px"}
+            />
             <Flex flexDir={"column"}>
-              <Text>Previsão de entrega</Text>
-              <Text>20 min - 30 min</Text>
+              <Text fontFamily="Roboto, sans-serif" fontSize={"16px"}>
+                Previsão de entrega
+              </Text>
+              <Text fontFamily="Roboto, sans-serif" fontSize={"16px"}>
+                20 min - 30 min
+              </Text>
             </Flex>
           </Flex>
-          <Flex>
-            <Icon as={RiMoneyDollarCircleLine} boxSize={"40px"} mr={"5px"} />
+          <Flex alignItems={"center"}>
+            <Icon
+              as={BiSolidDollarCircle}
+              boxSize={"25px"}
+              bgColor={"#C47F17"}
+              color={"white"}
+              borderRadius={"20px"}
+              mr={"5px"}
+              p={"4px"}
+            />
             <Flex flexDir={"column"}>
-              <Text>Pagamento na entrega</Text>
+              <Text fontFamily="Roboto, sans-serif" fontSize={"16px"}>
+                Pagamento na entrega
+              </Text>
               <Text>
                 {dataDelivery?.payment === 0
                   ? "Cartão de crédito"
